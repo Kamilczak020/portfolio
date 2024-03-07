abadb58926f7a6f1985d1c4d2f3307ad9d46f8d2
Kamil Solecki <kamilczak020@gmail.com>
2024-03-07 20:52:32 +0100

    fix moon rotation

--- src/components/Moon/index.tsx
+++ src/components/Moon/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import React, { ComponentType, Suspense } from 'react';
+import React, { ComponentType, Suspense, useEffect, useState } from 'react';
 import { mustExist } from '@penumbra/util/exists';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import classnames from 'classnames/bind';
+import { usePathname } from 'next/navigation';
 import style from './style.module.scss';
 
 const cx = classnames.bind(style);
@@ -14,7 +15,6 @@ type MoonPhase = (typeof phaseValues)[number];
 
 export type MoonProps = {
   phase: MoonPhase;
-  rotation?: number;
   className?: string;
 };
 
@@ -22,17 +22,46 @@ const moons: Map<number, ComponentType<any>> = new Map(
   phaseValues.map((phase) => [phase, dynamic(() => import(`./moon-phase-${phase}.svg`))]),
 );
 
-export function Moon({ phase, rotation, className }: MoonProps): JSX.Element {
+type AnimationProperties = {
+  direction: 1 | -1;
+  duration: number;
+};
+
+export const moonAnimations: Record<MoonPhase, AnimationProperties> = {
+  1: { direction: 1, duration: 30 },
+  2: { direction: 1, duration: 35 },
+  3: { direction: -1, duration: 25 },
+  4: { direction: 1, duration: 25 },
+  5: { direction: -1, duration: 30 },
+};
+
+export function Moon({ phase, className }: MoonProps): JSX.Element {
   const MoonComponent = mustExist(moons.get(phase));
   const classNames = cx(style.moonContainer, className);
 
+  const [rotation, setRotation] = useState(0);
+  const controls = useAnimation();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setRotation((prev) => prev + 90);
+  }, [pathname]);
+
+  useEffect(() => {
+    const rotate = async () => {
+      const { direction, duration } = moonAnimations[phase];
+      await controls.start({
+        rotate: [rotation, rotation + 360 * direction],
+        transition: { duration, repeat: Infinity, ease: 'linear' },
+      });
+    };
+
+    rotate();
+  }, [controls, rotation]);
+
   return (
     <Suspense fallback={<div className={classNames} />}>
-      <motion.div
-        className={classNames}
-        animate={{ rotate: rotation }}
-        transition={{ type: 'tween' }}
-      >
+      <motion.div className={classNames} animate={controls}>
         <MoonComponent className={style.moon} />
       </motion.div>
     </Suspense>
--- src/components/MoonBar/MoonBarMoons/index.tsx
+++ src/components/MoonBar/MoonBarMoons/index.tsx
@@ -1,10 +1,8 @@
 'use client';
 
+import React from 'react';
 import { Moon } from '@penumbra/components/Moon';
-import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { usePathname } from 'next/navigation';
-import { useTicker } from '@penumbra/hooks/ticker';
 import style from './style.module.scss';
 
 export default function MoonBarMoons() {
@@ -13,27 +11,14 @@ export default function MoonBarMoons() {
   }
 
   const root = document?.getElementById('moon-bar-overlay') || document?.body;
-  const [rotation, setRotation] = useState(0);
-  const pathname = usePathname();
-
-  const degRot = 10;
-  const tickerTime = useTicker(10);
-
-  useEffect(() => {
-    setRotation((prev) => prev + 90);
-  }, [pathname]);
-
-  useEffect(() => {
-    setRotation((prev) => prev + degRot);
-  }, [tickerTime]);
 
   return createPortal(
     <div className={style.moons}>
-      <Moon phase={1} className={style.moon} rotation={rotation} />
-      <Moon phase={2} className={style.moon} rotation={rotation} />
-      <Moon phase={3} className={style.moon} rotation={rotation} />
-      <Moon phase={4} className={style.moon} rotation={rotation} />
-      <Moon phase={5} className={style.moon} rotation={rotation} />
+      <Moon phase={1} className={style.moon} />
+      <Moon phase={2} className={style.moon} />
+      <Moon phase={3} className={style.moon} />
+      <Moon phase={4} className={style.moon} />
+      <Moon phase={5} className={style.moon} />
     </div>,
     root,
   );
