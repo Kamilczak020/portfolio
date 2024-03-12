813ba1c88fea0c4617811d6e6f6ddb7fc373467d
Kamil Solecki <kamilczak020@gmail.com>
2024-03-12 23:46:12 +0100

    fix contact form

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,5 @@
-import { NumberRange } from '@penumbra/util/types';
-import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
-export const metadata: Metadata = {
-  title: 'Penumbra | Home',
-  description: 'Penumbra',
-};
-
-export type ProjectMetadata = {
-  title: string;
-  display: string;
-  slug: string;
-  time: NumberRange;
-};
-
-export default function HomePage() {
+export function HomePage() {
   return redirect('/home/brevy-support');
 }
--- src/app/home/projects.ts
+++ src/app/home/projects.ts
@@ -3,7 +3,14 @@ import { metadata as brevyVideo } from '@penumbra/app/home/brevy-video/page.mdx'
 import { metadata as cblRadio } from '@penumbra/app/home/cbl-radio/page.mdx';
 import { metadata as decorway } from '@penumbra/app/home/decorway/page.mdx';
 import { metadata as elampki } from '@penumbra/app/home/elampki/page.mdx';
-import { ProjectMetadata } from '@penumbra/app/home/page';
+import { NumberRange } from '@penumbra/util/types';
+
+export type ProjectMetadata = {
+  title: string;
+  display: string;
+  slug: string;
+  time: NumberRange;
+};
 
 export const projects: Array<ProjectMetadata> = [
   brevySupport,
